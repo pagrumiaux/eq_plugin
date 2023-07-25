7cf8769c1f6e2270982f113caead8aa437c66e47
Pierre-Amaury Grumiaux <pierreamaury.grumiaux@gmail.com>
2023-07-25 23:27:42 +0200

    added sliders to GUI

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -175,8 +175,8 @@ bool SimpleEQ22AudioProcessor::hasEditor() const
 
 juce::AudioProcessorEditor* SimpleEQ22AudioProcessor::createEditor()
 {
-    //return new SimpleEQ22AudioProcessorEditor (*this);
-    return new juce::GenericAudioProcessorEditor(*this);
+    return new SimpleEQ22AudioProcessorEditor (*this);
+    //return new juce::GenericAudioProcessorEditor(*this);
 }
 
 //==============================================================================
@@ -185,12 +185,21 @@ void SimpleEQ22AudioProcessor::getStateInformation (juce::MemoryBlock& destData)
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+
+    juce::MemoryOutputStream mos(destData, true);
+    apvts.state.writeToStream(mos);
 }
 
 void SimpleEQ22AudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+    if (tree.isValid())
+    {
+        apvts.replaceState(tree);
+        updateFilters();
+    }
 }
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
